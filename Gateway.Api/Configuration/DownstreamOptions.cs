78637164f5127629fb65c204be58cc088ce23d61
Bismark Agyare <bismarkagyare54@gmail.com>
2026-08-29 21:09:26 +0000

    feat: refactor DownstreamOptions to use DownstreamRoute for route configuration

--- Gateway.Api/Configuration/DownstreamOptions.cs
+++ Gateway.Api/Configuration/DownstreamOptions.cs
@@ -1,6 +1,13 @@
 namespace Gateway.Api.Configuration;
 
+public class DownstreamRoute
+{
+    public string Path { get; set; } = string.Empty;
+
+    public string Target { get; set; } = string.Empty;
+}
+
 public class DownstreamOptions
 {
-    public string ProductsServiceUrl { get; set; } = string.Empty;
+    public List<DownstreamRoute> Routes { get; set; } = new();
 }
