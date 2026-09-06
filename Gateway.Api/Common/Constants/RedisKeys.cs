b3e1eb995cad8501f52f5cdd25c7e6387b7f55e6
Bismark Agyare <bismarkagyare54@gmail.com>
2026-09-06 20:50:52 +0000

    feat: add ApiKeyMetadata method for generating API key metadata keys

--- Gateway.Api/Common/Constants/RedisKeys.cs
+++ Gateway.Api/Common/Constants/RedisKeys.cs
@@ -3,4 +3,6 @@ namespace Gateway.Api.Common.Constants;
 public static class RedisKeys
 {
     public static string RateLimit(string apiKey) => $"ratelimit:{apiKey}";
+
+    public static string ApiKeyMetadata(string apiKey) => $"apikey:meta:{apiKey}";
 }
